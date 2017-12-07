ffc27a15b7ad6ff28cf98eafeb250b1012c0a091
Kanit Sharma <kanitshrm09@gmail.com>
2017-12-07 16:49:15 +0530

    proxy fallback added

--- peer-server.js
+++ peer-server.js
@@ -1,2 +1,6 @@
 var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: process.env.PORT || 9000, path: '/peerjs'});
+var server = PeerServer({
+  proxied: true,
+  port: process.env.PORT || 9000,
+  path: '/peerjs'
+});
